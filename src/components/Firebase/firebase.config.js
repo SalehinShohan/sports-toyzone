000bb980da2dde0f0c31044a51c8f1f8324dfb60
SalehinShohan <ps.shohan1@gmail.com>
2023-05-18 10:34:38 +0600

    add env.local in firebase

--- src/components/Firebase/firebase.config.js
+++ src/components/Firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD5wj7vMEyQaiSBdXEeaUc6lKMeblPXRqs",
-  authDomain: "sports-toyzone.firebaseapp.com",
-  projectId: "sports-toyzone",
-  storageBucket: "sports-toyzone.appspot.com",
-  messagingSenderId: "110514601837",
-  appId: "1:110514601837:web:78785e3d694f8186646464"
+  apiKey:import.meta.env.VITE_apiKey,
+  authDomain:import.meta.env.VITE_authDomain,
+  projectId:import.meta.env.VITE_projectId,
+  storageBucket:import.meta.env.VITE_storageBucket,
+  messagingSenderId:import.meta.env.VITE_messagingSenderId,
+  appId:import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
